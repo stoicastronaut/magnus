46316a9947ae27f27e952b4a7aaa3c6976909040
Gerardo Lankenau <glankenau@hey.com>
2026-03-25 19:57:28 +0100

    Create Settings in rust backend and Add Form in React
    This commit creates a Settings struct that handles the password
    and the urls to communicate with Claude. It also includes the
    Form to fill up in React.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,38 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use tauri::Manager;
+mod config;
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[tauri::command]
+fn get_settings(app: tauri::AppHandle) -> Result<config::Settings, String> {
+    let path = app.path().app_data_dir()
+        .map_err(|e| e.to_string())?
+        .join("settings.json");
+    config::Settings::load(&path)
+}
+
+#[tauri::command]
+fn save_settings(app: tauri::AppHandle, api_key: String, base_url: String) -> Result<(), String> {
+    let path = app.path().app_data_dir()
+        .map_err(|e| e.to_string())?
+        .join("settings.json");
+    let settings = config::Settings{
+        api_key: api_key,
+        base_url: base_url,
+    };
+    settings.save(&path)?;
+    Ok(())
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, get_settings, save_settings])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
