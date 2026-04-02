e34857f0e9efc4ae61cdd309eb46e18069b8b953
Gerardo Lankenau <glankenau@hey.com>
2026-04-02 13:33:41 +0200

    Add linter and formatter to rust
    This commits adds a linter and a formatter to the rust
    codebase, as well as the changes after running them.

--- backend/src/chats.rs
+++ backend/src/chats.rs
@@ -1,4 +1,4 @@
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 use std::fs;
 use std::path::Path;
 
@@ -21,14 +21,16 @@ impl Chat {
         fs::create_dir_all(chats_dir).map_err(|e| e.to_string())?;
         let filename = format!("{}-{}.json", self.created_at, self.id);
         let path = chats_dir.join(filename);
-        let str_pretty = serde_json::to_string_pretty(&self).map_err(|e| e.to_string())?;
+        let str_pretty =
+            serde_json::to_string_pretty(&self).map_err(|e| e.to_string())?;
         fs::write(path, str_pretty).map_err(|e| e.to_string())?;
         Ok(())
     }
 
-    pub fn load(path: &Path) -> Result<Chat, String>{
+    pub fn load(path: &Path) -> Result<Chat, String> {
         let json_str = fs::read_to_string(path).map_err(|e| e.to_string())?;
-        let json_pretty = serde_json::from_str(&json_str).map_err(|e| e.to_string())?;
+        let json_pretty =
+            serde_json::from_str(&json_str).map_err(|e| e.to_string())?;
         Ok(json_pretty)
     }
 
